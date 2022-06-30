6711cce7ddee18dedb572b4c5bb348175078923a
Dimitri Lagast <lagast.dimitri@gmail.com>
2022-07-01 00:24:47 +0200

    Cleanup PlayerCamera class

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -1,19 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Camera : MonoBehaviour
+public class PlayerCamera : MonoBehaviour
 {
     public float moveSpeed = 1f;
     private float defaultMoveFactor = 0.01f; // Used to lower move speed, so we don't have to work with small decimals.
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         Move();
