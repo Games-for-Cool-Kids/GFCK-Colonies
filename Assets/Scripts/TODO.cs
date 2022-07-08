26d1895abfdd35183685a9f03a4fe3404a6b86cf
Dimitri Lagast <lagast.dimitri@gmail.com>
2022-07-08 17:40:59 +0200

    Add some todos

--- Assets/Scripts/TODO.cs
+++ Assets/Scripts/TODO.cs
@@ -2,8 +2,8 @@
  * To Do:
  * - Account for center of selected object when dragging it, so it only jumps up. Now it seems to go to the point of raycast hit. Should only go up.
  * - Smooth dragging. Move towards mouse instead of being on top of mouse.
- * 
- * 
+ * - Highlight object that hand hovers
+ * - Have both options to toss object into stockpile or to release mouse on top of stockpile. Toss because it's fun, on mouse release to avoid object being stuck.
  * 
  */
 
