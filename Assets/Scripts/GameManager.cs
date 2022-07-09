7b8ec75989327f16f7711ad0975af9c5b414d997
Dimitri Lagast <lagast.dimitri@gmail.com>
2022-07-09 17:46:50 +0200

    Make game manager a singleton and allow access to commonly needed components

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,42 @@
 using UnityEngine;
 
-
 public class GameManager : MonoBehaviour
 {
-    void Start()
-    {
+    public PlayerCamera PlayerCamera { get; private set; }
+    public HandController HandController { get; private set; }
 
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            Debug.Assert(_instance != null);
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        _instance = this;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        GameObject mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        Debug.Assert(mainCamera != null);
+
+        // Store player camera.
+        var playerCamera = mainCamera.GetComponent<PlayerCamera>();
+        Debug.Assert(playerCamera != null);
+
+        PlayerCamera = playerCamera;
+
+        // Store hand controller.
+        var handController = mainCamera.GetComponent<HandController>();
+        Debug.Assert(handController != null);
+
+        HandController = handController;
+    }
+
     void Update()
     {
         
