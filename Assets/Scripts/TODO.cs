25a06ca51d0baebf03f0c0c5ca96762f9fb9de17
Dimitri Lagast <lagast.dimitri@gmail.com>
2022-07-15 17:02:42 +0200

    Use hacknplan instead of todo file

--- Assets/Scripts/TODO.cs
+++ Assets/Scripts/TODO.cs
@@ -1,13 +1,13 @@
 /*
  * To Do:
- * - Account for center of selected object when dragging it, so it only jumps up. Now it seems to go to the point of raycast hit. Should only go up.
- * - Smooth dragging. Move towards mouse instead of being on top of mouse.
+ * - 
  * - Highlight object that hand hovers
- * - Have both options to toss object into stockpile or to release mouse on top of stockpile. Toss because it's fun, on mouse release to avoid object being stuck.
+ * - 
  * - ResourceDisplay
  *      + Sort resources in ResourceDisplay
  *      + Move ResourceDisplay towards camera instead of just up.
  *      + Only draw when hovered
- *      + Show empty text when hovered?
+ *      + Show empty placeholder text when hovered?
+ * - Create physics material(s) for draggable objects.
  */
 
