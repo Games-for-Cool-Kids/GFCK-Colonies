40c9e681fc49703e567e228c7ce1f1caf3c769e4
Dimitri Lagast <lagast.dimitri@gmail.com>
2022-07-18 01:27:34 +0200

    Add singleton base class.

---
+++ Assets/Scripts/Util/MonoBehaviourSingleton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MonoBehaviourSingleton<T> : MonoBehaviour where T : Component
+{
+	public static T Instance { get; private set; }
+
+	public virtual void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this as T;
+			DontDestroyOnLoad(this);
+		}
+		else
+		{
+			Debug.LogError("Only one instance of singleton allowed. Duplicate exists on object: " + this.name);
+			Destroy(gameObject); // Only one object of type allowed.
+		}
+	}
+}
