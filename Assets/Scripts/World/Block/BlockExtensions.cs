5e860b7ddc8abe0118b83e22b6d01bb7eb92d740
Dimitri Lagast <lagast.dimitri@gmail.com>
2022-10-26 09:34:16 +0200

    Make some functions block extension methods.

--- Assets/Scripts/World/Block/BlockExtensions.cs
+++ Assets/Scripts/World/Block/BlockExtensions.cs
@@ -14,6 +14,12 @@ namespace World
             return block.worldPosition + Vector3.up / 2;
         }
 
+        public static bool IsSolidBlock(this BlockData block)
+        {
+            return block != null
+                && block.type != BlockType.AIR;
+        }
+
         public static bool IsBuildable(this BlockData block)
         {
             return block != null
@@ -21,6 +27,50 @@ namespace World
                 && block.type != BlockType.WATER
                 && block.type != BlockType.SNOW
                 && block.type != BlockType.AIR;
+        }
+
+        public static bool HasNeighbor(this BlockData block, GameWorldChunkData worldChunks, BlockAdjacency direction)
+        {
+            Vector3 offset = Vector3.zero;
+            switch (direction)
+            {
+                case BlockAdjacency.NORTH:
+                    offset = Vector3.forward;
+                    break;
+                case BlockAdjacency.SOUTH:
+                    offset = Vector3.back;
+                    break;
+                case BlockAdjacency.WEST:
+                    offset = Vector3.left;
+                    break;
+                case BlockAdjacency.EAST:
+                    offset = Vector3.right;
+                    break;
+                case BlockAdjacency.ABOVE:
+                    offset = Vector3.up;
+                    break;
+                case BlockAdjacency.BELOW:
+                    offset = Vector3.down;
+                    break;
+            }
+
+            var neighbor = worldChunks.GetBlockAt(block.worldPosition + offset);
+            return neighbor.IsSolidBlock();
+        }
+
+        public static bool IsSurfaceBlock(this BlockData block, ChunkData chunk)
+        {
+            BlockData blockAbove = chunk.GetBlockAdjacentTo(block, BlockAdjacency.ABOVE);
+            return blockAbove == null
+                || blockAbove.type == BlockType.AIR;
+        }
+
+        public static bool IsWalkable(this BlockData block, ChunkData chunk)
+        {
+            return block.IsSurfaceBlock(chunk)
+                && block.type != BlockType.AIR
+                && block.type != BlockType.WATER
+                && block.passable; // Determines if it can be used for pathfinding.
         }
     }
 }
