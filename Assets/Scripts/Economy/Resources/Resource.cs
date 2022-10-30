6df6bef3152febcfbba36a532d78217590151de9
Dimitri Lagast <lagast.dimitri@gmail.com>
2022-10-30 18:54:36 +0100

    Move enum out of class.

--- Assets/Scripts/Economy/Resources/Resource.cs
+++ Assets/Scripts/Economy/Resources/Resource.cs
@@ -1,12 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public enum ResourceType
-{
-    RESOURCE_INVALID = -1,
-    RESOURCE_WOOD = 0,
-    RESOURCE_STONE = 1
-}
 
 public class Resource : MonoBehaviour
 {
---
+++ Assets/Scripts/Economy/Resources/ResourceType.cs
@@ -0,0 +1,6 @@
+﻿public enum ResourceType
+{
+    RESOURCE_INVALID = -1,
+    RESOURCE_WOOD = 0,
+    RESOURCE_STONE = 1
+}
