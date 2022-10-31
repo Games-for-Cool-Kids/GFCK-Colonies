31f9ee00468975b2dfdd97574c901f1b9cc8495f
Dimitri Lagast <lagast.dimitri@gmail.com>
2022-10-31 23:01:42 +0100

    Remove unneeded comments/code.

--- Assets/Scripts/Buildings/LumberMill.cs
+++ Assets/Scripts/Buildings/LumberMill.cs
@@ -2,24 +2,8 @@ using Jobs;
 
 public class LumberMill : Building
 {
-    // Create lumberjack
-    // -> lumberjack creates chop-wood job
-    //          chop job = move to tree + chop/take log + return log
-    //               job has list of sub-jobs that are looped over.
-    //               job has a loop bool.
-    // -> assign villager to job
-    // -> building has unused resources
-    //      -> building creates pick up resources job
-
-    public override void Start()
+    void Start()
     {
-        base.Start();
-
         AddJob(JobType.LUMBERJACK);
-    }
-
-    void Update()
-    {
-        
     }
 }
