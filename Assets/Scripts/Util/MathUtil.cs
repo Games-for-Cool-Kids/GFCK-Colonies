362cc8d563d09922129b7dc33463ee5c7dacc6e3
Dimitri Lagast <lagast.dimitri@gmail.com>
2022-11-01 14:43:18 +0100

    Store the distance and squared diagonal distance between blocks.
     Can be used for distance comparisons.

---
+++ Assets/Scripts/Util/MathUtil.cs
@@ -0,0 +1,6 @@
+﻿
+public static class MathUtil
+{
+    public const float DIAG_DIST_BETWEEN_BLOCKS = 1.41f * 1.41f;
+    public const float SQRD_DIAG_DIST_BETWEEN_BLOCKS = DIAG_DIST_BETWEEN_BLOCKS * DIAG_DIST_BETWEEN_BLOCKS;
+}
