0f31f7a511d9a052b8cab3287481c2292f9f46af
Dimitri Lagast <lagast.dimitri@gmail.com>
2022-11-05 00:44:34 +0100

    Resource stacks to track resource instances.

---
+++ Assets/Scripts/Economy/Resources/ResourceStack.cs
@@ -0,0 +1,14 @@
+﻿namespace Economy
+{
+    public class ResourceStack
+    {
+        public ResourceType type = ResourceType.RESOURCE_INVALID;
+        public int amount = 0;
+
+        public ResourceStack(ResourceType type, int amount)
+        {
+            this.type = type;
+            this.amount = amount;
+        }
+    }
+}
