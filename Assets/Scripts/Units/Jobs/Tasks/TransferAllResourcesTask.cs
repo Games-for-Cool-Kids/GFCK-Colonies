c92eb45dd28de8392d9008aeb3ae4c779fe41821
Dimitri Lagast <lagast.dimitri@gmail.com>
2022-11-09 21:05:38 +0100

    Create task to transfer entire inventory.

---
+++ Assets/Scripts/Units/Jobs/Tasks/TransferAllResourcesTask.cs
@@ -0,0 +1,17 @@
+﻿namespace Jobs
+{
+    public class TransferAllResourcesTask : TransferResourcesTask
+    {
+        public TransferAllResourcesTask(Job job, TaskFlag flags = TaskFlag.None) : base(job, flags)
+        { }
+
+        public override void Start()
+        {
+            base.Start();
+
+            // Add all resources in unit inventory.
+            foreach(var resource in job.GetAssignedUnit().inventory.storedResources)
+                resourcesToTransfer.Add(new(resource.Key, resource.Value));
+        }
+    }
+}
