1228be42f3164e2c94faafb2ab08bf8f1ea49639
Dimitri Lagast <lagast.dimitri@gmail.com>
2022-11-11 15:05:46 +0100

    Define values directly and explain them in comments.

--- Assets/Scripts/Util/MathUtil.cs
+++ Assets/Scripts/Util/MathUtil.cs
@@ -1,6 +1,6 @@
 ﻿
 public static class MathUtil
 {
-    public const float DIAG_DIST_BETWEEN_BLOCKS = 1.41f * 1.41f;
-    public const float SQRD_DIAG_DIST_BETWEEN_BLOCKS = DIAG_DIST_BETWEEN_BLOCKS * DIAG_DIST_BETWEEN_BLOCKS;
+    public const float DIAG_DIST_BETWEEN_BLOCKS = 1.41f * 1.41f; // 1.9881
+    public const float SQRD_DIAG_DIST_BETWEEN_BLOCKS = DIAG_DIST_BETWEEN_BLOCKS * DIAG_DIST_BETWEEN_BLOCKS; // 3.95254161
 }
