2c7d765f17052890d765d7d46716a04600802844
Dimitri Lagast <lagast.dimitri@gmail.com>
2022-11-11 16:23:22 +0100

    Change transfer all to deliver all task.

---
+++ Assets/Scripts/Units/Jobs/Tasks/DeliverAllResourcesTask.cs
@@ -0,0 +1,20 @@
+﻿namespace Jobs
+{
+    public class DeliverAllResourcesTask : TransferResourcesTask
+    {
+        public DeliverAllResourcesTask(Job job,
+            TaskFlag flags = TaskFlag.None)
+                : base(job, TransferType.Delivery, flags)
+        { }
+
+        public override void Start()
+        {
+            // Add all resources in unit inventory.
+            foreach (var resource in job.GetAssignedUnit().inventory.storedResources)
+                if (resource.Value > 0)
+                    resourcesToTransfer.Add(new(resource.Key, resource.Value));
+
+            base.Start();
+        }
+    }
+}
