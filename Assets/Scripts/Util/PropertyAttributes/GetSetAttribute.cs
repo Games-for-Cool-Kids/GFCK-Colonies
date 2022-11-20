3d5d6b97af8e2f65cc117517f0b165a701925e33
Dimitri Lagast <lagast.dimitri@gmail.com>
2022-11-20 18:51:43 +0100

    Some comments for our custom attributes.

--- Assets/Scripts/Util/PropertyAttributes/GetSetAttribute.cs
+++ Assets/Scripts/Util/PropertyAttributes/GetSetAttribute.cs
@@ -1,4 +1,8 @@
 ﻿using UnityEngine;
+
+/// <summary>
+/// [GetSet] attribute for properties in scripts so we can edit them in the inspector. (In Unity normally only fields can be visible/edited in the inspector.)
+/// </summary>
 public sealed class GetSetAttribute : PropertyAttribute
 {
     public readonly string name;
--- Assets/Scripts/Util/PropertyAttributes/TagSelectorAttribute.cs
+++ Assets/Scripts/Util/PropertyAttributes/TagSelectorAttribute.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// [TagSelector] attribute for a tags dropdown editor for scripts. (Like the one to set a GameObject's tag in inspector.)
+/// </summary>
 public class TagSelectorAttribute : PropertyAttribute
 {
     public bool UseDefaultTagFieldDrawer = false;
